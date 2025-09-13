0a598612296ea0ac1a8d7347baeb28708c8514b0
Lulzim Morina <morinalulzim.fds@gmail.com>
2025-09-13 12:43:26 +0200

    Ändere Dateinamen und übersetze Kategorisierungen in Englisch

--- sorter.py
+++ sorter.py
@@ -2,24 +2,24 @@
 # Learning Goals: Lists, Loops, Conditionals, String Methods
 
 # Example list of files to sort
-files = ["bild1.jpg", "text1.txt", "lied1.mp3", "film1.mp4", "unbekannt.xyz"]
+files = ["image1.jpg", "text1.txt", "song1.mp3", "video1.mp4", "unknown.xyz"]
 
 # File type categories as an example
-bilder = [".jpg", ".png"]
-dokumente = [".txt", ".pdf"]
-musik = [".mp3", ".wav"]
+images = [".jpg", ".png"]
+documents = [".txt", ".pdf"]
+music = [".mp3", ".wav"]
 videos = [".mp4", ".mov"]
 
 # Sort and categorize files
 for file in files:
     # Check file extension and categorize
-    if file.endswith(tuple(bilder)):
-        print(file, "gehört zu: BILDER")
-    elif file.endswith(tuple(dokumente)):
-        print(file, "gehört zu: DOKUMENTE")
-    elif file.endswith(tuple(musik)):
-        print(file, "gehört zu: MUSIK")
+    if file.endswith(tuple(images)):
+        print(file, "belongs to: IMAGES")
+    elif file.endswith(tuple(documents)):
+        print(file, "belongs to: DOCUMENTS")
+    elif file.endswith(tuple(music)):
+        print(file, "belongs to: MUSIC")
     elif file.endswith(tuple(videos)):
-        print(file, "gehört zu: VIDEOS")
+        print(file, "belongs to: VIDEOS")
     else:
-        print(file, "gehört zu: UNBEKANNT")
+        print(file, "belongs to: UNKNOWN")
